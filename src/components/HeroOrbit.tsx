74e0ee180b085746bbdf61cc8ba537df08b61566
AnhChienVu <vuanhchien003@gmail.com>
2025-05-12 17:42:55 -0600

    Make the Stars animate

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -8,18 +8,20 @@ function HeroOrbit({
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
       <div
-        className=""
+        className="flex items-start justify-start animate-spin [animation-duration:20s]"
         style={{
           height: `${size}px`,
           width: `${size}px`,
           transform: `rotate(${rotation}deg)`,
         }}
       >
-        <div
-          className=" inline-flex"
-          style={{ transform: `rotate(${rotation * -1}deg)` }}
-        >
-          {children}
+        <div className="animate-spin [animation-duration:20s]">
+          <div
+            className=" inline-flex"
+            style={{ transform: `rotate(${rotation * -1}deg)` }}
+          >
+            {children}
+          </div>
         </div>
       </div>
     </div>
